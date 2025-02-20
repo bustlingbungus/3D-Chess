f8aa7fcd439ac66c785e551aa499dc59efb10077
DarthM <mgracer48@yahoo.com>
2025-02-20 00:25:45 -0500

    King: Add castling special move
    also some formatting changes for other files

--- 3D Chess/Assets/Scripts/Board.cs
+++ 3D Chess/Assets/Scripts/Board.cs
@@ -16,7 +16,7 @@ public class Board : MonoBehaviour
     private GameObject cell_prefab;
     /// <summary> xyz dimensions of the 3D array. </summary>
     public Vector3Int grid_dimensions = new Vector3Int(8, 8, 8);
-    
+
 
     /// <summary> 3D array of cell objects. </summary>
     private Cell[,,] grid;
@@ -32,31 +32,31 @@ public class Board : MonoBehaviour
     void Start()
     {
         // intialise array
-        grid = new Cell[grid_dimensions.x,grid_dimensions.y,grid_dimensions.z];
+        grid = new Cell[grid_dimensions.x, grid_dimensions.y, grid_dimensions.z];
 
         // iterate over every (x,y,z) coordinate triple. Instantiate a new cell for each coordinate. 
-        for (int x=0; x<grid_dimensions.x; x++) 
+        for (int x = 0; x < grid_dimensions.x; x++)
         {
             // Cell positions will begin at -7 on each axis, range until 7, and have a step of 2.
             // this will make the board centred at (0,0,0)
-            float xpos = (2f*x)-7f;
-            for (int y=0; y<grid_dimensions.y; y++) 
+            float xpos = (2f * x) - 7f;
+            for (int y = 0; y < grid_dimensions.y; y++)
             {
-                float ypos = (2f*y)-7f;
-                for (int z=0; z<grid_dimensions.z; z++) 
+                float ypos = (2f * y) - 7f;
+                for (int z = 0; z < grid_dimensions.z; z++)
                 {
                     // Instantiate cell prefab as a child of cell container, and access the Cell script
                     Cell cell = Instantiate(
-                        cell_prefab, 
-                        new Vector3(xpos,ypos,(2f*z)-7f), 
-                        Quaternion.identity, 
+                        cell_prefab,
+                        new Vector3(xpos, ypos, (2f * z) - 7f),
+                        Quaternion.identity,
                         transform.GetChild(0))
                             .GetComponent<Cell>();
                     // set the cell's indices
                     cell.index = new Vector3Int(x, y, z);
 
                     // commit hte finished cell to the array 
-                    grid[cell.index.x,cell.index.y,cell.index.z] = cell;
+                    grid[cell.index.x, cell.index.y, cell.index.z] = cell;
                 }
             }
         }
@@ -67,10 +67,11 @@ public class Board : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (regen_moves) {
+        if (regen_moves)
+        {
             RegenerateMoves();
             regen_moves = false;
-        }   
+        }
     }
 
     /// <summary>
@@ -78,7 +79,8 @@ public class Board : MonoBehaviour
     /// </summary>
     /// <param name="index">xyz Vector3Int of the desired xyz indices</param>
     /// <returns>A reference to the cell at indices xyz. <c>null</c> if the indices are out of bounds.</returns>
-    public Cell GetCellAt(Vector3Int index) {
+    public Cell GetCellAt(Vector3Int index)
+    {
         return GetCellAt(index.x, index.y, index.z);
     }
 
@@ -90,8 +92,8 @@ public class Board : MonoBehaviour
     public Cell GetCellAt(int x, int y, int z)
     {
         // return null if index is out of bounds
-        if (x<0||y<0||z<0 || x>=grid_dimensions.x||y>=grid_dimensions.y||z>=grid_dimensions.z) return null;
-        return grid[x,y,z];
+        if (x < 0 || y < 0 || z < 0 || x >= grid_dimensions.x || y >= grid_dimensions.y || z >= grid_dimensions.z) return null;
+        return grid[x, y, z];
     }
 
 
@@ -103,25 +105,30 @@ public class Board : MonoBehaviour
     /// Toggles the index rendering for all cells in the grid.
     /// </summary>
     [ContextMenu("Toggle Indices")]
-    public void ToggleIndices() {
+    public void ToggleIndices()
+    {
         foreach (Cell cell in grid) cell.ToggleIndexDisplay();
     }
 
     [ContextMenu("Regenerate Moves")]
-    public void RegenerateMoves() {
-        foreach (Cell cell in grid) {
-            cell.attackers = new Dictionary<TeamColour,List<Piece>>();
+    public void RegenerateMoves()
+    {
+        foreach (Cell cell in grid)
+        {
+            cell.attackers = new Dictionary<TeamColour, List<Piece>>();
             // i know it says initialisation can be simplified, but trust me the alternative is ugly and less simple.
             cell.attackers.Add(TeamColour.White, new List<Piece>());
             cell.attackers.Add(TeamColour.Black, new List<Piece>());
         }
         GameObject[] pieces = GameObject.FindGameObjectsWithTag("Piece");
         Stack<King> kings = new Stack<King>(); // kings will be handled after the other pieces, as this is necessary for how they avoid moving into check
-        foreach (GameObject o in pieces) {
+        foreach (GameObject o in pieces)
+        {
             Piece p = o.GetComponent<Piece>();
-            if (p.Type==PieceType.King) kings.Push(p as King);
+            if (p.Type == PieceType.King) kings.Push(p as King);
             p.RegenerateMoves();
         }
         while (kings.Count > 0) kings.Pop().RefineMoves();
     }
+
 }
--- 3D Chess/Assets/Scripts/Pieces/King.cs
+++ 3D Chess/Assets/Scripts/Pieces/King.cs
@@ -12,57 +12,81 @@ public class King : Piece
 
     public void RefineMoves()
     {
-            TeamColour opposite = Colour==TeamColour.White?TeamColour.Black:TeamColour.White;
-            for (int i=0; i<available_moves.Count; i++) {
-                MoveInfo m = available_moves[i];
-                if (m.cell.attackers[opposite].Count>0) available_moves.RemoveAt(i--);
-            }
+        TeamColour opposite = Colour == TeamColour.White ? TeamColour.Black : TeamColour.White;
+        for (int i = 0; i < available_moves.Count; i++)
+        {
+            MoveInfo m = available_moves[i];
+            if (m.cell.attackers[opposite].Count > 0) available_moves.RemoveAt(i--);
+        }
     }
+
 
 
     public override List<Cell> find_valid_moves()
     {
         List<Cell> res = new List<Cell>();
-        TeamColour opposite_colour = Colour==TeamColour.White?TeamColour.Black:TeamColour.White;
+        TeamColour opposite_colour = Colour == TeamColour.White ? TeamColour.Black : TeamColour.White;
 
         // orthogonal movements
-        for (int i=0; i<6; i++)
+        for (int i = 0; i < 6; i++)
         {
             // find displacement by i's value
-            Vector3Int disp = new Vector3Int(i<2?1:0,i<4&&i>1?1:0,i<6&&i>3?1:0);
-            if (i%2==1) disp *= -1;
+            Vector3Int disp = new Vector3Int(i < 2 ? 1 : 0, i < 4 && i > 1 ? 1 : 0, i < 6 && i > 3 ? 1 : 0);
+            if (i % 2 == 1) disp *= -1;
 
             // check current displacement
-            Cell curr = _board.GetCellAt(Cell.index+disp);
-            if (curr != null) {
+            Cell curr = _board.GetCellAt(Cell.index + disp);
+            if (curr != null)
+            {
                 curr.attackers[Colour].Add(this);
-                if (curr.occupant==null||curr.occupant.Colour!=Colour) res.Add(curr);
+                if (curr.occupant == null || curr.occupant.Colour != Colour) res.Add(curr);
             }
         }
 
         // diagonals
-        for (int i=0; i<8; i++)
+        for (int i = 0; i < 8; i++)
         {
             // find displacement with binary counting
-            Vector3Int disp = new Vector3Int((i&4)!=0?1:-1,(i&2)!=0?1:-1,(i&1)!=0?1:-1);
+            Vector3Int disp = new Vector3Int((i & 4) != 0 ? 1 : -1, (i & 2) != 0 ? 1 : -1, (i & 1) != 0 ? 1 : -1);
 
             // check current displacement
-            Cell curr = _board.GetCellAt(Cell.index+disp);
-            if (curr != null) {
+            Cell curr = _board.GetCellAt(Cell.index + disp);
+            if (curr != null)
+            {
                 curr.attackers[Colour].Add(this);
-                if (curr.occupant==null||curr.occupant.Colour!=Colour) res.Add(curr);
+                if (curr.occupant == null || curr.occupant.Colour != Colour) res.Add(curr);
             }
         }
 
         // 2 dimensional diagonals
-        for (int i=0; i<12; i++)
+        for (int i = 0; i < 12; i++)
         {
-            Vector3Int disp = new Vector3Int(i<8?i<4?-1:1:0,i<8?(i%2)==1?(i%4)==1?-1:1:0:i<10?-1:1,i<8?(i%2)==0?(i%4)==0?-1:1:0:(i%2)==0?-1:1);
+            Vector3Int disp = new Vector3Int(i < 8 ? i < 4 ? -1 : 1 : 0, i < 8 ? (i % 2) == 1 ? (i % 4) == 1 ? -1 : 1 : 0 : i < 10 ? -1 : 1, i < 8 ? (i % 2) == 0 ? (i % 4) == 0 ? -1 : 1 : 0 : (i % 2) == 0 ? -1 : 1);
             // check current displacement
-            Cell curr = _board.GetCellAt(Cell.index+disp);
-            if (curr != null) {
+            Cell curr = _board.GetCellAt(Cell.index + disp);
+            if (curr != null)
+            {
                 curr.attackers[Colour].Add(this);
-                if (curr.occupant==null||curr.occupant.Colour!=Colour) res.Add(curr);
+                if (curr.occupant == null || curr.occupant.Colour != Colour) res.Add(curr);
+            }
+        }
+        if (move_cnt == 0)
+        {
+            // castling
+            for (int i = 0; i < 2; i++)
+            {
+                Vector3Int disp = new Vector3Int(i == 0 ? 2 : -2, 0, 0);
+                Cell curr = _board.GetCellAt(Cell.index + disp);
+                if (curr != null && curr.occupant == null)
+                {
+                    bool can_castle = true;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Cell check = _board.GetCellAt(Cell.index + new Vector3Int(i == 0 ? 1 : -1, 0, 0) * j);
+                        if (check != null && check.attackers[opposite_colour].Count > 0) can_castle = false;
+                    }
+                    if (can_castle) res.Add(curr);
+                }
             }
         }
 
